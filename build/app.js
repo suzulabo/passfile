85a04e5d5141c339a09f9a3551dbc3782b21a77e
kenji suzuki <for-reg@suzulabo.net>
2020-05-22 11:12:49 +0900

    Updates

--- build/app.js
+++ build/app.js
@@ -125,7 +125,7 @@ var StyleNode=function(){this.start=0,this.end=0,this.previous=null,this.parent=
 
   var resourcesUrl = scriptElm ? scriptElm.getAttribute('data-resources-url') || scriptElm.src : '';
   var start = function() {
-    var url = new URL('./p-1495ba01.system.js', resourcesUrl);
+    var url = new URL('./p-276496ab.system.js', resourcesUrl);
     System.import(url.href);
   };
 
